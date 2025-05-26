abb0ac5e42fda8ee52ad00ec2115b9a2f56e924a
danyrfreitas <danyrfreitas@gmail.com>
2025-05-26 22:18:40 +0000

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { useState } from "react"
+import { Exemplo1 } from "./components/Exemplo1"
+import { Exemplo2 } from "./components/Exemplo2"
+import { Header } from "./components/Header"
 
 export function App() {
-  return 
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Exemplo1 />
+        <Exemplo2 />
+      </main>
+    </>
+  )
 }
